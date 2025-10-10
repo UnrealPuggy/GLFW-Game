52ebbfbed14eb5d1b2b188ed49e925e380cc3854
UnrealPug <126524020+UnrealPuggy@users.noreply.github.com>
2025-10-09 20:19:45 -0600

    skate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include "pug.rgb.h"
 int main(void)
 {
     GLFWwindow *window;
@@ -20,12 +21,28 @@ int main(void)
     /* Make the window's context current*/
     glfwMakeContextCurrent(window);
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    // Image?
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // Set wrapping/filtering options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 8, 8, 0, GL_RGB, GL_UNSIGNED_BYTE, src_pug_rgb);
     /* Loop until the user closes the window asd*/
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClearColor(.1, .1, .1, 1);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnable(GL_TEXTURE_2D);
         glBegin(GL_TRIANGLES);
         glColor3f(1.0f, 0.0f, 0.0f); // Red
         glVertex2f(-0.5f, -0.5f);
@@ -34,6 +51,7 @@ int main(void)
         glColor3f(0.0f, 0.0f, 1.0f); // Blue
         glVertex2f(0.0f, 0.5f);
         glEnd();
+        glDisable(GL_TEXTURE_2D);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
