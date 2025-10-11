899e779d3ac881dd14217929e43948bdfc4d7e5d
Puggers <126524020+UnrealPuggy@users.noreply.github.com>
2025-10-10 21:21:18 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -129,6 +129,7 @@ int main()
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
         // Tells OpenGL to use your linked vertex+fragment shaders for the next draw.
         glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glUseProgram(shaderProgram);
